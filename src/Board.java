fe73851a8c00ab4732fe818ffb02ad4eecb26799
zaneboga <25zboga@athenian.org>
2024-12-16 20:47:12 -0800

    added more vertices

--- src/Board.java
+++ src/Board.java
@@ -10,12 +10,21 @@ public class Board {
         frame.setLayout(new BorderLayout());
 
         DijkstraGraph graph = new DijkstraGraph();
-        graph.addVertex("A");
-        graph.addVertex("B");
-        graph.addVertex("C");
-        graph.addEdge("A", "B", 5);
-        graph.addEdge("B", "C", 2);
-        graph.addEdge("A", "C", 10);
+        for (char label = 'A'; label <= 'J'; label++) {
+            graph.addVertex(String.valueOf(label));
+        }
+        graph.addEdge("A", "B", 4);
+        graph.addEdge("A", "C", 2);
+        graph.addEdge("B", "D", 7);
+        graph.addEdge("C", "D", 3);
+        graph.addEdge("C", "E", 8);
+        graph.addEdge("D", "F", 5);
+        graph.addEdge("E", "F", 1);
+        graph.addEdge("F", "G", 6);
+        graph.addEdge("G", "H", 9);
+        graph.addEdge("H", "I", 3);
+        graph.addEdge("I", "J", 4);
+        graph.addEdge("E", "J", 2);
 
         JPanel sidePanel = new JPanel();
         sidePanel.setPreferredSize(new Dimension(300, 1000));
